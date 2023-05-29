0f79635120d5efd3cc5568fbdf53d6883ac840aa
Ray Alcides <railayli2@hotmail.com>
2023-05-29 17:22:49 -0300

    segundo dia de estudos em testes

--- cypress/integration/CAC.TAT.spec.js
+++ cypress/integration/CAC.TAT.spec.js
@@ -8,14 +8,42 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
   });
 
-  it.only('preencha os campos obrigatórios e envia o formulário', () => {
+  it('preencha os campos obrigatórios e envia o formulário', () => {
+    const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste'
     cy.get('#firstName').type('Raiani')
     cy.get('#lastName').type('Alcides')
     cy.get('#email').type('railayli@exemplo.com')
-    cy.get('#open-text-area').type('teste')
+    cy.get('#open-text-area').type(longText, {delay: 0})
     cy.get('button[type="submit"]').click()
 
     cy.get('.success').should('be.visible')
+  })
+
+  it('Mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('Raiani')
+    cy.get('#lastName').type('Alcides')
+    cy.get('#email').type('railayli@exemplo,com')
+    cy.get('#open-text-area').type('teste')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  it('Campo de telefone continua vazio quando preenchido com valor não-numérico', () =>{
+    cy.get('#phone')
+     .type('abcdefghij')
+     .should('have.value', '')
+  })
+
+  it('Exibe mansagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () =>{
+    cy.get('#firstName').type('Raiani')
+    cy.get('#lastName').type('Alcides')
+    cy.get('#email').type('railayli@exemplo.com')
+    cy.get('#phone-checkbox').click()
+    cy.get('#open-text-area').type('teste')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error').should('be.visible')
   })
 });
 
